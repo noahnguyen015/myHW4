f5c2426ff98bc01cdb70aa09d30d545588baf416
Noah Nguyen <noahn1105@MSI.localdomain>
2023-10-04 21:22:20 -0700

    12th commit

--- ladder.py
+++ ladder.py
@@ -1,10 +1,10 @@
-def my_steps(num):
+def mysteps(num):
     
     if num <= 2:
       return num 
     
-    if num > 25:
-      return 0
+    if num > 25 or num < 1:
+      raise ValueError
 
     list = [0] * (num)
 
@@ -15,7 +15,6 @@ def my_steps(num):
       list[i] = list[i-1] + list[i-2]
 
     numofways = list[num-1]
-
 
     return numofways
 
