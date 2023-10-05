ab6e789cb3d0b71df19560c48c6bc148e0c392f5
Noah Nguyen <noahn1105@MSI.localdomain>
2023-10-04 20:59:45 -0700

    10th commit

--- bug1.py
+++ bug1.py
@@ -1,36 +1,36 @@
+
 class Base:
   def __init__(self, x, size):
     self.x = x
     self.size = size
 
 
-class Square(Base):
+class Circle(Base):
   def __init__(self, x, y, size):
       super().__init__(x, size)
       self.y = y
 
   def shape(self):
-      return print('this is a circle')
+      return 'this is a circle'
   def draw(self):
-      return print(f"""({self.x},{self.y}) 
-{self.size}
-          , - ~ ~ ~ - ,
-      , '                ' ,
-    ,                        ,
-   ,                          ,
-  ,                            ,
-  ,                            ,
-  ,                            ,
-  ,                            ,
-   ,                          ,
-    ,                        ,  
-      ,                   , '
-        ' - ,  _ _ _  , '
-                """)
+      return f"""({self.x},{self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
+
   
 def main():
-  s = Square(2,2,1)
-  s.shape()
-  s.draw()
+  s = Circle(1,2,3)
+  print(s.shape())
+  print(s.draw())
 
 main()
